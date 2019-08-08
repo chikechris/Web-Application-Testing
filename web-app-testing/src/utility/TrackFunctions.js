435b3818f1fdf14fab85a5ffa31598cdb20d298f
Chike Chris <cchike2017@gmail.com>
2019-08-08 17:34:16 -0400

    building out TrackFunc.js and TrackFunc.test.js files

--- web-app-testing/src/utility/TrackFunctions.js
+++ web-app-testing/src/utility/TrackFunctions.js
@@ -1,7 +1,23 @@
 export const trackHit = () => {};
 
-export const trackStrike = () => {};
+export const trackStrike = (strike, callBackStrike, callBackBall) => {
+  if (strike === 3) {
+    callBackStrike(0);
+    callBackBall(0);
+    return [callBackStrike(0), callBackBall(0)];
+  }
+  strike += 1;
+  return callBackStrike(strike);
+};
 
-export const trackBall = () => {};
+export const trackBall = (ball, callBackBall, callBackStrike) => {
+  if (ball === 4) {
+    callBackStrike(0);
+    callBackBall(0);
+    return [callBackBall(0), callBackStrike(0)];
+  }
+  const newBall = ball + 1;
+  return callBackBall(newBall);
+};
 
 export const trackFoul = () => {};
--- web-app-testing/src/utility/TrackFunctions.test.js
+++ web-app-testing/src/utility/TrackFunctions.test.js
@@ -4,10 +4,21 @@ import "@testing-library/react/cleanup-after-each";
 
 import { trackBall, trackHit, trackStrike, trackFoul } from "./TrackFunctions";
 
-describe("Testing utilities functions for the track handlers", () => {
-  describe("Testing trackBall function", () => {
+describe("Testing utilities functions for cthe track handlers", () => {
+  describe("Testing handleBall function", () => {
     it("Will increment react useState hook by one", () => {
-      expect(handleBall(0, value => value + 1)).toBe(1);
+      expect(trackBall(0, value => value, value => value)).toBe(1);
+    });
+  });
+
+  describe("Testing the trackStrike function", () => {
+    it("Will increment react useState hook by one", () => {
+      expect(trackStrike(0, value => value)).toBe(1);
+    });
+    it("Will reset handleBall and handleStrike hooks if maximum is reached", () => {
+      const ball = 3;
+      const strike = 3;
+      expect();
     });
   });
 });
