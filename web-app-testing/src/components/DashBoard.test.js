649dfebb35cf35a8dc5a3192deeae38446d694d4
Chike Chris <cchike2017@gmail.com>
2019-08-08 17:50:07 -0400

    built out the DashBoard and DashBoard.test components

---
+++ web-app-testing/src/components/DashBoard.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import DashBoard from "./DashBoard";
+import { render } from "@testing-library/react";
+import "@testing-library/react/cleanup-after-each";
+
+describe("DashBoard", () => {
+  it("renders without crashing", () => {
+    render(<DashBoard />);
+  });
+});
